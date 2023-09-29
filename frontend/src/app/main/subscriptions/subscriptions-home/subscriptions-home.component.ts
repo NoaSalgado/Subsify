785aeef1d8f8cbb3430fda18962a30f8435b1b5d
Noa <noasalgadodev@gmail.com>
2023-09-29 14:24:12 +0200

    Add renewal date column

--- frontend/src/app/main/subscriptions/subscriptions-home/subscriptions-home.component.ts
+++ frontend/src/app/main/subscriptions/subscriptions-home/subscriptions-home.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
 @Component({
-  selector: 'app-subscriptions-home',
-  templateUrl: './subscriptions-home.component.html',
-  styleUrls: ['./subscriptions-home.component.css']
+  selector: "app-subscriptions-home",
+  templateUrl: "./subscriptions-home.component.html",
+  styleUrls: ["./subscriptions-home.component.css"],
 })
 export class SubscriptionsHomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  public renovationDate = this.calculateRenovationDate;
+  calculateRenovationDate(rowData: Array<any>): Date {
+    let aux: Date = new Date(rowData["SUBS_START_DATE"]);
+    let frecuencia: number = rowData["SUBS_FREQUENCY"];
+    aux.setMonth(aux.getMonth() + frecuencia);
+    /*switch(frecuencia){
+      case 1:
+      aux.setDate(aux.getMonth() + 1)
+            break;
+      case 2:
+      aux.setDate(aux.getMonth() + 3)
+            break;
+      case 3:
+      aux.setDate(aux.getMonth() + 12)
+            break;
+      default: 1;
+    }*/
+    return aux;
   }
-
+  constructor() {}
+  ngOnInit() {}
 }
