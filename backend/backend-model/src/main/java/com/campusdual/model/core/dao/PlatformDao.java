56e8fff9518d6daeb46473288d7a26dd118c2932
JulianOjea <32637786+JulianOjea@users.noreply.github.com>
2023-09-29 11:10:45 +0200

    Renamed Dao attrs

--- backend/backend-model/src/main/java/com/campusdual/model/core/dao/PlatformDao.java
+++ backend/backend-model/src/main/java/com/campusdual/model/core/dao/PlatformDao.java
@@ -9,5 +9,7 @@ import org.springframework.stereotype.Repository;
 @Lazy
 @ConfigurationFile(configurationFile = "dao/PlatformDao.xml", configurationFilePlaceholder = "dao/placeholders.properties")
 public class PlatformDao extends OntimizeJdbcDaoSupport {
-    public static final String ATTR_NAME = "NAME";
+
+    public static final String ATTR_PLATF_ID = "PLATF_ID";
+    public static final String ATTR_NAME = "PLATF_NAME";
 }
--- backend/backend-model/src/main/java/com/campusdual/model/core/dao/SubscriptionDao.java
+++ backend/backend-model/src/main/java/com/campusdual/model/core/dao/SubscriptionDao.java
@@ -9,12 +9,12 @@ import org.springframework.stereotype.Repository;
 @Lazy
 @ConfigurationFile(configurationFile = "dao/SubscriptionDao.xml", configurationFilePlaceholder = "dao/placeholders.properties")
 public class SubscriptionDao extends OntimizeJdbcDaoSupport {
-    public static final String ATTR_SUBS_ID = "ID_SUBS";
-    public static final String ATTR_PRICE = "PRICE";
-    public static final String ATTR_FREQUENCY = "FREQUENCY";
-    public static final String ATTR_ACTIVE = "ACTIVE";
-    public static final String ATTR_START_DATE = "START_DATE";
-    public static final String ATTR_END_DATE = "END_DATE";
-    public static final String ATTR_PLATFORM = "PLATFORM";
-    public static final String ATTR_USER = "USER";
+    public static final String ATTR_SUBS_ID = "SUBS_ID";
+    public static final String ATTR_PRICE = "SUBS_PRICE";
+    public static final String ATTR_FREQUENCY = "SUBS_FREQUENCY";
+    public static final String ATTR_ACTIVE = "SUBS_ACTIVE";
+    public static final String ATTR_START_DATE = "SUBS_START_DATE";
+    public static final String ATTR_END_DATE = "SUBS_END_DATE";
+    public static final String ATTR_PLATFORM_ID = "PLATF_ID";
+    public static final String ATTR_USER = "USER_";
 }
