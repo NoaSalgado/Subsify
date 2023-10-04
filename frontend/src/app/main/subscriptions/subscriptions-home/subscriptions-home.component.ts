a05c7a3cbd16e2eaa86919b5cabf0d93b23ff5f4
Noa <noasalgadodev@gmail.com>
2023-10-04 11:27:56 +0200

    Fix renewal date visualization

--- frontend/src/app/main/subscriptions/subscriptions-home/subscriptions-home.component.ts
+++ frontend/src/app/main/subscriptions/subscriptions-home/subscriptions-home.component.ts
@@ -5,25 +5,6 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./subscriptions-home.component.css"],
 })
 export class SubscriptionsHomeComponent implements OnInit {
-  public renovationDate = this.calculateRenovationDate;
-  calculateRenovationDate(rowData: Array<any>): Date {
-    let aux: Date = new Date(rowData["SUBS_START_DATE"]);
-    let frecuencia: number = rowData["SUBS_FREQUENCY"];
-    aux.setMonth(aux.getMonth() + frecuencia);
-    /*switch(frecuencia){
-      case 1:
-      aux.setDate(aux.getMonth() + 1)
-            break;
-      case 2:
-      aux.setDate(aux.getMonth() + 3)
-            break;
-      case 3:
-      aux.setDate(aux.getMonth() + 12)
-            break;
-      default: 1;
-    }*/
-    return aux;
-  }
   constructor() {}
   ngOnInit() {}
 }
