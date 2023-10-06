984c61720052b17a06272f659f1b4f7233d95773
Noa <noasalgadodev@gmail.com>
2023-10-06 09:21:35 +0200

    Update subscription dao

--- backend/backend-model/src/main/java/com/campusdual/model/core/dao/SubscriptionDao.java
+++ backend/backend-model/src/main/java/com/campusdual/model/core/dao/SubscriptionDao.java
@@ -10,12 +10,8 @@ import org.springframework.stereotype.Repository;
 @ConfigurationFile(configurationFile = "dao/SubscriptionDao.xml", configurationFilePlaceholder = "dao/placeholders.properties")
 public class SubscriptionDao extends OntimizeJdbcDaoSupport {
     public static final String ID = "SUBS_ID";
-    public static final String PRICE = "SUBS_PRICE";
     public static final String FREQUENCY = "FR_ID";
     public static final String ACTIVE = "SUBS_AUTORENEWAL";
-    public static final String START_DATE = "SUBS_START_DATE";
-    public static final String END_DATE = "SUBS_END_DATE";
     public static final String PLATF_ID = "PLATF_ID";
     public static final String USER = "USER_";
-    public static final String QUERY_UPDATE = "updateSubs";
 }
