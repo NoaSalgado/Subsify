f860e8dd4843f831ef97af5155ff87d8859baf69
Noa <noasalgadodev@gmail.com>
2023-10-10 13:58:00 +0200

    Add custom styles and login background image

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,34 +1,41 @@
-import { NgModule } from '@angular/core';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { APP_CONFIG, ONTIMIZE_MODULES, ONTIMIZE_PROVIDERS, OntimizeWebModule } from 'ontimize-web-ngx';
+import { NgModule } from "@angular/core";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import {
+  APP_CONFIG,
+  ONTIMIZE_MODULES,
+  ONTIMIZE_PROVIDERS,
+  OntimizeWebModule,
+} from "ontimize-web-ngx";
 
-import { environment } from '../environments/environment';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CONFIG } from './app.config';
+import { environment } from "../environments/environment";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CONFIG } from "./app.config";
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from "@angular/material";
 
 // Standard providers...
 // Defining custom providers (if needed)...
-export const customProviders: any = [
-];
+export const customProviders: any = [];
 
 @NgModule({
   imports: [
     ONTIMIZE_MODULES,
     OntimizeWebModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register("ngsw-worker.js", {
+      enabled: environment.production,
+    }),
   ],
-  declarations: [
-    AppComponent
-  ],
-  bootstrap: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
   providers: [
     { provide: APP_CONFIG, useValue: CONFIG },
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: "fill" },
+    },
     ONTIMIZE_PROVIDERS,
-    ...customProviders
+    ...customProviders,
   ],
 })
-export class AppModule { }
+export class AppModule {}
