6175b1a13915c94053930e2a98cbfe8148ef79bb
mirela <panait.mire@gmail.com>
2023-10-17 09:05:53 +0200

    Add category security

--- backend/backend-model/src/main/java/com/campusdual/model/core/service/CategoryService.java
+++ backend/backend-model/src/main/java/com/campusdual/model/core/service/CategoryService.java
@@ -5,9 +5,11 @@ import com.campusdual.api.core.service.ICategoryService;
 import com.campusdual.model.core.dao.CategoryDao;
 import com.ontimize.jee.common.dto.EntityResult;
 import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
+import com.ontimize.jee.common.security.PermissionsProviderSecured;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,21 +26,25 @@ public class CategoryService implements ICategoryService {
     private DefaultOntimizeDaoHelper daoHelper;
 
     @Override
+    @Secured({ PermissionsProviderSecured.SECURED })
     public EntityResult categoryQuery(Map<String, Object> keysValues, List<String> attributes) throws OntimizeJEERuntimeException {
         return this.daoHelper.query(this.categoryDao, keysValues, attributes);
     }
 
     @Override
+    @Secured({ PermissionsProviderSecured.SECURED })
     public EntityResult categoryInsert(Map<String, Object> attributes) throws OntimizeJEERuntimeException {
         return this.daoHelper.insert(this.categoryDao, attributes);
     }
 
     @Override
+    @Secured({ PermissionsProviderSecured.SECURED })
     public EntityResult categoryUpdate(Map<String, Object> attributes, Map<String, Object> KeyValues) throws OntimizeJEERuntimeException {
         return null;
     }
 
     @Override
+    @Secured({ PermissionsProviderSecured.SECURED })
     public EntityResult categoryDelete(Map<String, Object> keyValues) throws OntimizeJEERuntimeException {
         return this.daoHelper.delete(this.categoryDao, keyValues);
     }
