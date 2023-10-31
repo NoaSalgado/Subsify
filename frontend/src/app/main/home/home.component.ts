ae27397e8e094e3c04af9f96348456666a3f6777
Daniel Martinez <danmc018@gmail.com>
2023-10-31 13:34:19 +0100

    Fixed cards in the Home Menu

--- frontend/src/app/main/home/home.component.ts
+++ frontend/src/app/main/home/home.component.ts
@@ -1,17 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { OListComponent } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
+@ViewChild('list',{static:false}) list:OListComponent
+public remainingDays: Array<number>;
 
   constructor(
     private router: Router,
     private actRoute: ActivatedRoute
   ) {
+  }
+
+  calculateDays() {
+    
+    const endDates = this.list.dataArray.map(sub=> sub.SUB_LAPSE_END);
+    this.remainingDays= endDates.map(date=>{
+        const datediff = new Date(date).getTime()- new Date().getTime()
+        const remainingDays = Math.ceil(datediff/(1000*60*60*24))
+        return remainingDays
+    })
+
   }
 
   ngOnInit() {
--- frontend/src/app/main/home/home.module.ts
+++ frontend/src/app/main/home/home.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from "@angular/core";
+import { NgModule, LOCALE_ID } from "@angular/core";
+import es from '@angular/common/locales/es';
 import { OntimizeWebModule } from "ontimize-web-ngx";
 import { SharedModule } from "../../shared/shared.module";
 import { HomeRoutingModule } from "./home-routing.module";
 import { HomeComponent } from "./home.component";
+import { registerLocaleData } from '@angular/common';
 
+registerLocaleData(es);
 @NgModule({
   imports: [SharedModule, OntimizeWebModule, HomeRoutingModule],
   declarations: [HomeComponent],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'es-ES' // 'de-DE' for Germany, 'fr-FR' for France ...
+  },
+]
 })
 export class HomeModule {}
