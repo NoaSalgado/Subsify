d0621dab9073c16c3ce3ec2c43166baf91ed402f
JulianOjea <32637786+JulianOjea@users.noreply.github.com>
2023-10-31 10:53:48 +0100

    Add shared user list

--- frontend/src/app/main/subscriptions/subscriptions-details/subscriptions-details.component.ts
+++ frontend/src/app/main/subscriptions/subscriptions-details/subscriptions-details.component.ts
@@ -1,73 +1,16 @@
-import {
-  AfterViewInit,
-  Component,
-  Injector,
-  OnInit,
-  ViewChild,
-} from "@angular/core";
-import { OFormComponent, OntimizeService } from "ontimize-web-ngx";
-
+import { AfterViewInit, Component, Injector, OnInit, ViewChild } from "@angular/core";
 @Component({
   selector: "app-subscriptions-details",
   templateUrl: "./subscriptions-details.component.html",
   styleUrls: ["./subscriptions-details.component.css"],
 })
-export class SubscriptionsDetailsComponent implements AfterViewInit, OnInit {
-  @ViewChild("subscriptionDetailForm", { static: false })
-  subscriptionDetailForm: OFormComponent;
-  @ViewChild("sharedUsersForm", { static: false })
-  sharedUsersForm: OFormComponent;
-  protected service: OntimizeService;
-  private sharedSubs;
+
+export class SubscriptionsDetailsComponent implements OnInit {
 
   constructor(protected injector: Injector) {
-    this.service = this.injector.get(OntimizeService);
+
   }
 
   ngOnInit(): void {
-    this.configureService();
-  }
-
-  protected configureService(): void {
-    const conf = this.service.getDefaultServiceConfiguration("sharedSubs");
-    this.service.configureService(conf);
-  }
-
-  getSubscriptionCount(subID) {
-    if (this.service !== null) {
-      const filter = {
-        SUBS_ID: subID,
-      };
-      this.service
-        .query(filter, ["SHARED_SUB_USER"], "sharedSub")
-        .subscribe((resp) => {
-          if (resp.code === 0) {
-            this.updateSubLapsePrice(resp.data);
-          } else {
-            alert("Impossible to query data!");
-          }
-        });
-    }
-  }
-
-  ngAfterViewInit(): void {
-    this.sharedUsersForm.onFormInitStream.subscribe(() => {
-      this.sharedUsersForm.setInsertMode();
-    });
-  }
-
-  async transferData() {
-    const subID = this.subscriptionDetailForm.formData["SUBS_ID"].value;
-    this.sharedUsersForm.setFieldValue("SUBS_ID", subID);
-    this.getSubscriptionCount(subID);
-  }
-
-  updateSubLapsePrice(subsCount): void {
-    console.log(subsCount);
-    const price = this.subscriptionDetailForm.getFieldValue("SUB_LAPSE_PRICE");
-    this.subscriptionDetailForm.setFieldValue(
-      "SUB_LAPSE_PRICE",
-      price / (subsCount.length + 1)
-    );
   }
 }
