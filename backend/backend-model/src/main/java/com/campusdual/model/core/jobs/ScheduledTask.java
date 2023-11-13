2dcb8ad69f14c46cf20006611408b15fa619a33c
Noa <noasalgadodev@gmail.com>
2023-11-13 08:00:14 +0100

    Add sublapse custom insert and modify schedule task

--- backend/backend-model/src/main/java/com/campusdual/model/core/jobs/ScheduledTask.java
+++ backend/backend-model/src/main/java/com/campusdual/model/core/jobs/ScheduledTask.java
@@ -3,8 +3,10 @@ package com.campusdual.model.core.jobs;
 
 import com.campusdual.model.core.dao.*;
 import com.campusdual.model.core.service.PlanService;
+import com.campusdual.model.core.service.SubLapseCustomService;
 import com.campusdual.model.core.service.SubLapseService;
 import com.campusdual.model.core.service.SubscriptionService;
+import com.ontimize.jee.common.db.Entity;
 import com.ontimize.jee.common.db.SQLStatementBuilder;
 import com.ontimize.jee.common.dto.EntityResult;
 import com.ontimize.jee.common.dto.EntityResultMapImpl;
@@ -27,6 +29,9 @@ public class ScheduledTask {
 
     @Autowired
     SubscriptionService subscriptionService;
+
+    @Autowired
+    SubLapseCustomService subLapseCustomService;
     /*
     private SQLStatementBuilder.BasicExpression filterByActiveAndDate(String endDate, String active) {
         SQLStatementBuilder.BasicField endDateField = new SQLStatementBuilder.BasicField(endDate);
@@ -40,7 +45,7 @@ public class ScheduledTask {
 
 
     //Obtains new sub_lapse_price by getting active plans found by plan id
-    public Map<String,Object> getActivePlanByPlanId(Map<String, Object> planId){
+    public Map<String, Object> getActivePlanByPlanId(Map<String, Object> planId) {
         List<String> attrs = new ArrayList<>();
         attrs.add(PlanPriceDao.VALUE);
         attrs.add(PlanPriceDao.ID);
@@ -49,18 +54,40 @@ public class ScheduledTask {
     }
 
     //
-    public void updateSubsPlanPriceId(Map<String,Object> subsRegistry, Map<String,Object> newPlanPrice){
-        Map<String,Object> attributes = new HashMap<>();
+    public void updateSubsPlanPriceId(Map<String, Object> subsRegistry, Map<String, Object> newPlanPrice) {
+        Map<String, Object> attributes = new HashMap<>();
         attributes.put(SubscriptionDao.PLAN_PRICE_ID, newPlanPrice.get(PlanPriceDao.ID));
 
-        Map<String,Object> keyvalues = new HashMap<>();
+        Map<String, Object> keyvalues = new HashMap<>();
         keyvalues.put(SubscriptionDao.ID, subsRegistry.get(SubLapseDao.SUBS_ID));
 
         subscriptionService.subscriptionUpdate(attributes, keyvalues);
     }
 
-    @Scheduled(fixedRate=1000)
-    public void scheduleTask(){
+    public EntityResult getCustomSub(Map<String, Object> subsRegistry) {
+        List<String> attributes = Arrays.asList(
+                SubLapseCustomDao.SUBS_ID,
+                SubLapseCustomDao.SLC_PRICE,
+                SubLapseCustomDao.SLC_END
+        );
+        Map<String, Object> customSubQueryKV = new HashMap<>();
+        customSubQueryKV.put(SubLapseCustomDao.SUBS_ID, subsRegistry.get(SubLapseDao.SUBS_ID));
+        EntityResult customSubER = subLapseCustomService.subLapseCustomQuery(customSubQueryKV, attributes);
+        return customSubER;
+    }
+
+    public boolean hasCustomPrice(Map<String, Object> subsRegistry) {
+        EntityResult customSubER = this.getCustomSub(subsRegistry);
+        return customSubER.calculateRecordNumber() > 0;
+    }
+
+    public boolean keepCustomPrice(Map<String, Object> subsRegistry, Date renewDate) {
+        Map<String, Object> customSub = this.getCustomSub(subsRegistry).getRecordValues(0);
+        return customSub.get(SubLapseCustomDao.SLC_END) == null || ((Date) customSub.get(SubLapseCustomDao.SLC_END)).after(renewDate);
+    }
+
+    @Scheduled(fixedRate = 1000)
+    public void scheduleTask() {
         try {
 
             List<String> columns = Arrays.asList(
@@ -71,8 +98,8 @@ public class ScheduledTask {
                     SubLapseDao.PRICE,
                     PlanDao.FR_ID,
                     PlanDao.ID);
-                    //SubscriptionDao.PLAN_PRICE_ID,
-                    //PlanPriceDao.VALUE);
+            //SubscriptionDao.PLAN_PRICE_ID,
+            //PlanPriceDao.VALUE);
 
             EntityResult subscriptionsToUpdate = subLapseService.subLapseQueryRenewal(new HashMap<>(), columns);
 
@@ -83,19 +110,18 @@ public class ScheduledTask {
                 subscriptionsToUpdate.getRecordValues(0).get(SubLapseDao.PRICE)
                         .equals(subscriptionsToUpdate.getRecordValues(0).get(PlanPriceDao.VALUE));
             }*/
-            for(int i=0;i<erSize;i++){
+            for (int i = 0; i < erSize; i++) {
 
-                Map<String,Object> subsRegistry =  subscriptionsToUpdate.getRecordValues(i);
+                Map<String, Object> subsRegistry = subscriptionsToUpdate.getRecordValues(i);
 
-                Map<String,Object> planQueryAttrs=new HashMap<>();
+                Map<String, Object> planQueryAttrs = new HashMap<>();
                 planQueryAttrs.put(PlanDao.ID, subsRegistry.get(PlanDao.ID));
-                Map<String,Object> newPlanPrice = getActivePlanByPlanId(planQueryAttrs);
+                Map<String, Object> newPlanPrice = getActivePlanByPlanId(planQueryAttrs);
 
                 updateSubsPlanPriceId(subsRegistry, newPlanPrice);
 
-                Map<String,Object> attrs=new HashMap<>();
+                Map<String, Object> attrs = new HashMap<>();
                 attrs.put(SubLapseDao.SUBS_ID, subsRegistry.get(SubLapseDao.SUBS_ID));
-                attrs.put(SubLapseDao.PRICE, newPlanPrice.get(PlanPriceDao.VALUE));
                 attrs.put(PlanDao.FR_ID, subsRegistry.get(PlanDao.FR_ID));
 
                 java.sql.Date oldEndDate = (java.sql.Date) subsRegistry.get(SubLapseDao.END);
@@ -106,10 +132,17 @@ public class ScheduledTask {
                 Date newDate = Date.from((newDateLD.atStartOfDay(ZoneId.systemDefault()).toInstant()));
                 attrs.put(SubLapseDao.START, newDate);
 
-                subLapseService.subLapseInsert(attrs);
+                if (this.hasCustomPrice(subsRegistry) && this.keepCustomPrice(subsRegistry, newDate)) {
+                    EntityResult customSub = this.getCustomSub(subsRegistry);
+                    attrs.put(SubLapseDao.PRICE, customSub.getRecordValues(0).get(SubLapseCustomDao.SLC_PRICE));
+                } else {
+                    attrs.put(SubLapseDao.PRICE, newPlanPrice.get(PlanPriceDao.VALUE));
+                }
+                EntityResult result = subLapseService.subLapseInsert(attrs);
             }
 
-        } catch (Exception e) {
+        } catch (
+                Exception e) {
             e.printStackTrace();
             EntityResult res = new EntityResultMapImpl();
             res.setCode(EntityResult.OPERATION_WRONG);
