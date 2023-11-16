de74ae4e62298918ea5c1fe3bd1d68e618ea6dd5
mirela <panait.mire@gmail.com>
2023-11-16 11:23:43 +0100

    Add SubLapse query

--- backend/backend-model/src/main/java/com/campusdual/model/core/service/SubLapseService.java
+++ backend/backend-model/src/main/java/com/campusdual/model/core/service/SubLapseService.java
@@ -30,15 +30,20 @@ public class SubLapseService implements ISubLapseService {
     private SubscriptionService subscriptionService;
     @Autowired
     private PlanPriceService planPriceService;
+    @Autowired
+    private SubLapseService subLapseService;
+    @Autowired
+    private SubLapseCustomService subLapseCustomService;
 
     @Autowired
     private DefaultOntimizeDaoHelper daoHelper;
+
     @Override
     public EntityResult subLapseQuery(Map<String, Object> keysValues, List<String> attributes) throws OntimizeJEERuntimeException {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Map<String, Object> newKeyValues = new HashMap<>(keysValues);
         String username = authentication.getName();
-        newKeyValues.put(UserSubDao.USER,username);
+        newKeyValues.put(UserSubDao.USER, username);
 
         return this.daoHelper.query(this.subLapseDao, newKeyValues, attributes, SubLapseDao.ACTIVE_QUERY);
     }
@@ -50,7 +55,7 @@ public class SubLapseService implements ISubLapseService {
 
         Map<String, Object> newKeyValues = new HashMap<>();
         newKeyValues.put(UserSubDao.USER, username);
-        if(keysValues.containsKey(CategoryDao.NAME)){
+        if (keysValues.containsKey(CategoryDao.NAME)) {
             newKeyValues.put(CategoryDao.NAME, keysValues.get(CategoryDao.NAME));
         }
 
@@ -63,9 +68,9 @@ public class SubLapseService implements ISubLapseService {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Map<String, Object> newKeyValues = new HashMap<>(keysValues);
         String username = authentication.getName();
-        newKeyValues.put(UserSubDao.USER,username);
+        newKeyValues.put(UserSubDao.USER, username);
 
-        EntityResult subsToRenewEr =  this.daoHelper.query(this.subLapseDao, newKeyValues, attributes, SubLapseDao.SUBSCRIPTIONS_TO_RENEW);
+        EntityResult subsToRenewEr = this.daoHelper.query(this.subLapseDao, newKeyValues, attributes, SubLapseDao.SUBSCRIPTIONS_TO_RENEW);
         int erSize = subsToRenewEr.calculateRecordNumber();
         EntityResult newEntityResult = new EntityResultMapImpl(List.of(SubLapseDao.ID,
                 PlatformDao.NAME,
@@ -80,13 +85,13 @@ public class SubLapseService implements ISubLapseService {
                 PlanPriceDao.END,
                 PlanPriceDao.ID,
                 PlanDao.ID
-                ));
-        for(int i = 0; i <  erSize; i++){
-            Map<String, Object>  subRecord = subsToRenewEr.getRecordValues(i);
+        ));
+        for (int i = 0; i < erSize; i++) {
+            Map<String, Object> subRecord = subsToRenewEr.getRecordValues(i);
             Date planPriceEnd = (Date) subRecord.get(PlanPriceDao.END);
             Date subLapseEnd = (Date) subRecord.get(SubLapseDao.END);
             int planId = (int) subRecord.get(PlanDao.ID);
-            if(planPriceEnd != null && planPriceEnd.before(subLapseEnd) ){
+            if (planPriceEnd != null && planPriceEnd.before(subLapseEnd)) {
                 Map<String, Object> planPriceQueryKV = new HashMap<>();
                 planPriceQueryKV.put(PlanPriceDao.PLAN_ID, planId);
                 SQLStatementBuilder.BasicField planPriceEndField = new SQLStatementBuilder.BasicField(PlanPriceDao.END);
@@ -115,11 +120,11 @@ public class SubLapseService implements ISubLapseService {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Map<String, Object> newKeyValues = new HashMap<>(keysValues);
         String username = authentication.getName();
-        newKeyValues.put(UserSubDao.USER,username);
+        newKeyValues.put(UserSubDao.USER, username);
         return this.daoHelper.query(this.subLapseDao, newKeyValues, attributes, SubLapseDao.CHARTCATEGORY_QUERY);
     }
 
-    private int getFreq(Map<String, Object> attributes){
+    private int getFreq(Map<String, Object> attributes) {
         Map<String, Object> freqQuery = new HashMap<>();
         freqQuery.put(FrequencyDao.ID, attributes.get(FrequencyDao.ID));
         EntityResult freqER = this.frequencyService.frequencyQuery(freqQuery, List.of(FrequencyDao.VALUE));
@@ -143,11 +148,46 @@ public class SubLapseService implements ISubLapseService {
 
     @Override
     public EntityResult subLapseUpdate(Map<String, Object> attributes, Map<String, Object> keyValues) throws OntimizeJEERuntimeException {
-        if(attributes.containsKey(SubscriptionDao.ACTIVE)){
-            return subscriptionService.subscriptionUpdate(attributes,keyValues );
+
+        if (attributes.containsKey(SubscriptionDao.ACTIVE)) {
+            return subscriptionService.subscriptionUpdate(attributes, keyValues);
         }
+        Map<String, Object> subLapseKV = new HashMap<>();
+        int subLapseId = (int)keyValues.get(SubLapseDao.ID);
+        subLapseKV.put(SubLapseDao.ID, subLapseId);
+
+        EntityResult subLapseER = this.subLapseService.subLapseQuery(
+                subLapseKV,
+                List.of(SubscriptionDao.ID,
+                        SubLapseDao.START));
+        Map<String,Object> subsKV=new HashMap<>();
+        int subsId=(int)subLapseER.getRecordValues(0).get(SubscriptionDao.ID);
+        subsKV.put(SubscriptionDao.ID,subsId);
+        EntityResult subsER=this.subscriptionService.subscriptionQuery(subsKV,List.of(SubscriptionDao.PLAN_PRICE_ID));
+        Map<String,Object> planPriceKV=new HashMap<>();
+        planPriceKV.put(PlanPriceDao.ID,subsER.getRecordValues(0).get(SubscriptionDao.PLAN_PRICE_ID));
+
+        EntityResult standardPricePlanER = this.planPriceService.planPriceQuery(planPriceKV, List.of(PlanPriceDao.VALUE));
+        boolean isPriceCustomized = !Objects.equals(standardPricePlanER.getRecordValues(0).get(PlanPriceDao.VALUE),
+                new BigDecimal(String.valueOf(attributes.get(SubLapseDao.PRICE))));
+        if(isPriceCustomized){
+            Map<String, Object> attrCustomSubLapseInsert = new HashMap<>();
+            attrCustomSubLapseInsert.put(SubLapseCustomDao.SUBS_ID,subsId);
+            attrCustomSubLapseInsert.put(SubLapseCustomDao.SLC_PRICE, attributes.get(SubLapseDao.PRICE));
+            attrCustomSubLapseInsert.put(SubLapseCustomDao.SLC_START, subLapseER.getRecordValues(0).get(SubLapseDao.START));
+
+            if(attributes.containsKey(SubLapseCustomDao.SLC_END)){
+                attrCustomSubLapseInsert.put(SubLapseCustomDao.SLC_END, attributes.get(SubLapseCustomDao.SLC_END));
+            }
+            this.subLapseCustomService.subLapseCustomInsert(attrCustomSubLapseInsert);
+        }
+
+
+
+
         return this.daoHelper.update(this.subLapseDao, attributes, keyValues);
-    }
+
+        }
 
     @Override
     public EntityResult subLapseDelete(Map<String, Object> keyValues) throws OntimizeJEERuntimeException {
