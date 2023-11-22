bad7d8cafd10ee8806e4e3ba8144746b5d842291
Noa <noasalgadodev@gmail.com>
2023-11-22 22:58:20 +0100

    Fix donut chart bug

--- frontend/src/app/main/subscriptions/subscriptions-home/subscriptions-home.component.ts
+++ frontend/src/app/main/subscriptions/subscriptions-home/subscriptions-home.component.ts
@@ -1,44 +1,49 @@
 import { Component, Injector, OnInit, ViewChild } from "@angular/core";
 import { getMonthlyPricefunction } from "src/app/shared/shared.module";
-import { OChartComponent, DonutChartConfiguration } from 'ontimize-web-ngx-charts';
 import {
-Expression,
-FilterExpressionUtils,
-OComboComponent,
-OFilterBuilderComponent,
-OTableComponent,
-OTranslateService,
+  OChartComponent,
+  DonutChartConfiguration,
+} from "ontimize-web-ngx-charts";
+import {
+  Expression,
+  FilterExpressionUtils,
+  OComboComponent,
+  OFilterBuilderComponent,
+  OTableComponent,
+  OTranslateService,
 } from "ontimize-web-ngx";
 import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-selector: "app-subscriptions-home",
-templateUrl: "./subscriptions-home.component.html",
-styleUrls: ["./subscriptions-home.component.css"],
+  selector: "app-subscriptions-home",
+  templateUrl: "./subscriptions-home.component.html",
+  styleUrls: ["./subscriptions-home.component.css"],
 })
-
 export class SubscriptionsHomeComponent implements OnInit {
-@ViewChild("filterBuilderCat", { static: false })
-filterBuilderCat: OFilterBuilderComponent;
-@ViewChild("categories", { static: false }) categories: OComboComponent;
-@ViewChild("subLapseTable",{static:false}) subLapseTable:OTableComponent;
-@ViewChild("subLapseChart",{static:false}) subLapseChart:OChartComponent;
+  @ViewChild("filterBuilderCat", { static: false })
+  filterBuilderCat: OFilterBuilderComponent;
+  @ViewChild("categories", { static: false }) categories: OComboComponent;
+  @ViewChild("subLapseTable", { static: false }) subLapseTable: OTableComponent;
+  @ViewChild("subLapseChart", { static: false }) subLapseChart: OChartComponent;
 
-protected platformChart: OChartComponent;
-public chartData;
-protected chartParameters: DonutChartConfiguration;
-public getMonthlyPrice = getMonthlyPricefunction;
-user_role;
+  protected platformChart: OChartComponent;
+  public chartData;
+  protected chartParameters: DonutChartConfiguration;
+  public getMonthlyPrice = getMonthlyPricefunction;
+  user_role;
 
-constructor(protected injector: Injector,private router: Router,private actRoute: ActivatedRoute) {
+  constructor(
+    protected injector: Injector,
+    private router: Router,
+    private actRoute: ActivatedRoute
+  ) {
+    this.chartParameters = new DonutChartConfiguration();
+    this.chartParameters.showLabels = true;
+    this.chartParameters.cornerRadius = 0;
+    this.chartParameters.donutRatio = 0.1;
+  }
 
-this.chartParameters = new DonutChartConfiguration();
-this.chartParameters.showLabels = true;
-this.chartParameters.cornerRadius = 0;
-this.chartParameters.donutRatio = 0.1;
-}
-
-ngOnInit() {}
+  ngOnInit() {}
 
   ngAfterViewInit(): void {
     // this.subscriptionsSevice.registerTable(this.sublapseTable);
@@ -68,35 +73,41 @@ ngOnInit() {}
     }
   }
 
-  onDataLoaded2(event){
+  onDataLoaded2(event) {
     console.log(event);
     if (event && event.length > 0 && event[0].ROLENAME) {
-    
-    this.user_role=event[0].ROLENAME;
-    
-    if(this.user_role === 'admin'){
-      
-      this.router.navigate(['../../','plans'], {relativeTo: this.actRoute})
-    } else{
-      this.router.navigate(['../../','subscriptions'],{relativeTo:this.actRoute})
-    }
-  } else {
-    console.error('Invalid data received:', event);
-  }
+      this.user_role = event[0].ROLENAME;
 
+      if (this.user_role === "admin") {
+        this.router.navigate(["../../", "plans"], {
+          relativeTo: this.actRoute,
+        });
+      } else {
+        this.router.navigate(["../../", "subscriptions"], {
+          relativeTo: this.actRoute,
+        });
+      }
+    } else {
+      console.error("Invalid data received:", event);
+    }
   }
   resetFilter($event) {
     this.filterBuilderCat.triggerReload();
   }
 
-  setChartData($event){
+  setChartData($event) {
     console.log(this.subLapseChart.dataArray);
-      this.chartData=$event.map(subLapse=>{
-      const data={
-        x:subLapse.PLATF_NAME,
-        y:subLapse.shared_price/subLapse.FR_VALUE
-      }
+    this.chartData = $event.map((subLapse) => {
+      const data = {
+        x: subLapse.PLATF_NAME,
+        y: subLapse.shared_price / subLapse.FR_VALUE,
+      };
       return data;
-     });
+    });
+  }
+
+  onTabChange(event) {
+    console.log({ event });
+    (<any>this.subLapseChart).chartWrapper.update();
   }
 }
