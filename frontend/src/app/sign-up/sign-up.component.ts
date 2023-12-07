6f20eb0fbe2c7c1250ace58aa6a1830b4e06d132
Noa <noasalgadodev@gmail.com>
2023-12-07 13:52:58 +0100

    Fix sign-up component password input width

--- frontend/src/app/sign-up/sign-up.component.ts
+++ frontend/src/app/sign-up/sign-up.component.ts
@@ -1,23 +1,34 @@
-import { Component, Inject, Injector, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { AuthService, LocalStorageService, NavigationService, OFormComponent, OntimizeService } from 'ontimize-web-ngx';
-import { Observable } from 'rxjs';
+import {
+  Component,
+  Inject,
+  Injector,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation,
+} from "@angular/core";
+import {
+  AuthService,
+  LocalStorageService,
+  NavigationService,
+  OFormComponent,
+  OntimizeService,
+} from "ontimize-web-ngx";
 
 @Component({
-  selector: 'sign-up',
-  styleUrls: ['./sign-up.component.scss'],
-  templateUrl: './sign-up.component.html',
-  encapsulation: ViewEncapsulation.None
+  selector: "sign-up",
+  styleUrls: ["./sign-up.component.scss"],
+  templateUrl: "./sign-up.component.html",
+  encapsulation: ViewEncapsulation.None,
 })
-export class SignUpComponent implements OnInit{
+export class SignUpComponent implements OnInit {
   protected userService: OntimizeService;
-  @ViewChild("signUpForm", { static: false }) signUpForm: OFormComponent;
-  
+  @ViewChild("signUpForm", { static: false })
+  signUpForm: OFormComponent;
+
   constructor(
-    public injector: Injector, 
+    public injector: Injector,
     @Inject(AuthService) private authService: AuthService
-    ){
+  ) {
     this.userService = this.injector.get(OntimizeService);
   }
 
@@ -31,7 +42,7 @@ export class SignUpComponent implements OnInit{
     this.userService.configureService(conf);
   }
 
-  public async signUpUser(){
-      this.signUpForm.insert();
+  public async signUpUser() {
+    this.signUpForm.insert();
   }
 }
