9d96bc2f29de48b08d9dc37c72a60a624bc8426c
Noa <noasalgadodev@gmail.com>
2023-12-07 13:38:27 +0100

    Refactor user home component

--- frontend/src/app/main/user-home/user-home.component.ts
+++ frontend/src/app/main/user-home/user-home.component.ts
@@ -1,89 +1,97 @@
-import {
-  Component,
-  ElementRef,
-  Injector,
-  OnInit,
-  ViewChild,
-} from "@angular/core";
+import { Component, Injector, ViewChild } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
-import { OListComponent, OTranslateService, OntimizeService } from "ontimize-web-ngx";
-import { MatDialog } from "@angular/material/dialog";
-import { THIS_EXPR } from "@angular/compiler/src/output/output_ast";
+import { OListComponent, OTranslateService } from "ontimize-web-ngx";
 
 @Component({
   selector: "app-user-home",
   templateUrl: "./user-home.component.html",
   styleUrls: ["./user-home.component.css"],
 })
-export class UserHomeComponent implements OnInit {
+export class UserHomeComponent {
   @ViewChild("list", { static: false }) list: OListComponent;
-  public remainingDays: Array<string>;
-  public showRenewal: Array<boolean>;
-  public webLinks: Array<string>;
-  public priceToShow: Array<number>;
-  public incrementalPrice: Array<number>;
-  public decrementalPrice: Array<number>;
-  private service: OntimizeService;
-  public images: Array<string>;
-  public image = "data:image/png:./assets/images/subsify_round_logo.png";
-  public isListEmpty = false;
+  protected remainingDays: Array<string>;
+  protected showRenewal: Array<boolean>;
+  protected webLinks: Array<string>;
+  protected endDates: Array<Date>;
+  protected priceToShow: Array<number>;
+  protected incrementalPrice: Array<number>;
+  protected decrementalPrice: Array<number>;
+  protected images: Array<string>;
+  protected image = "data:image/png:./assets/images/subsify_round_logo.png";
+  protected isListEmpty = false;
 
-  constructor(
-    private router: Router,
-    private actRoute: ActivatedRoute,
-    private injector: Injector,
-    private translate: OTranslateService,
-  ) {
-    this.service = this.injector.get(OntimizeService);
-  }
-  onListDataLoaded(event){
-    this.isListEmpty = (event.length ===0);
+  constructor(private translate: OTranslateService) {}
 
-    this.calculateDays();
+  onListDataLoaded(event) {
+    this.isListEmpty = event.length === 0;
+    this.processData();
   }
 
-  calculateDays() {
+  processData() {
+    this.getSubscriptionsToCancel();
+    this.getWebLinks();
+    this.getEndDates();
+    this.getImages();
+    this.getRemainingDays();
+    this.getPriceToShow();
+    this.getIncrementalPrice();
+    this.getDecrementalPrice();
+  }
+
+  getSubscriptionsToCancel() {
     this.showRenewal = this.list.dataArray.map((sub) => !sub.SUBS_AUTORENEWAL);
+  }
+
+  getWebLinks() {
     this.webLinks = this.list.dataArray.map((sub) => sub.PLATF_LINK);
+  }
 
-    const endDates = this.list.dataArray.map((sub) => sub.SUB_LAPSE_END);
+  getEndDates() {
+    this.endDates = this.list.dataArray.map((sub) => sub.SUB_LAPSE_END);
+  }
 
+  getImages() {
     this.images = this.list.dataArray.map((sub, i, arr) => {
       if (!sub.PLATF_IMAGE)
         this.image = "./assets/images/subsify_round_logo.png";
       else this.image = `data:image/png;base64,${sub.PLATF_IMAGE.bytes}`;
-      console.log(i, this.image);
       return this.image;
     });
+  }
 
+  getRemainingDays() {
     const language = this.translate.getCurrentLang();
-    let tomorrow;
-    let today;
-    let In;
-    let days;
+    let tomorrow: string;
+    let today: string;
+    let preposition: string;
+    let days: string;
 
     if (language === "en") {
       tomorrow = "tomorrow";
       today = "today";
-      In = " in ";
+      preposition = " in ";
       days = " days";
-
-    }
-    else if (language === "es") {
+    } else if (language === "es") {
       tomorrow = "mañana";
       today = "hoy";
-      In = " en ";
+      preposition = " en ";
       days = " días";
     }
 
-    this.remainingDays = endDates.map((date) => {
+    this.remainingDays = this.endDates.map((date) => {
       const datediff = new Date(date).getTime() - new Date().getTime();
       const remainingDays = Math.ceil(datediff / (1000 * 60 * 60 * 24));
-      //const translation = 
-      //const remainingDaysString = "{{ "RENEW-ON" | oTranslate }}";
-      if (remainingDays === 1) { return tomorrow; } else if (remainingDays === 0) { return today; } return In + remainingDays.toString() + days;
-    });
 
+      if (remainingDays === 1) {
+        return tomorrow;
+      } else if (remainingDays === 0) {
+        return today;
+      }
+      return preposition + remainingDays.toString() + days;
+    });
+  }
+
+  getPriceToShow() {
     this.priceToShow = this.list.dataArray.map((sub) => {
       if (
         (sub.SLC_START && sub.SLC_END <= sub.SUB_LAPSE_END) ||
@@ -97,6 +105,9 @@ export class UserHomeComponent implements OnInit {
       }
       return sub.SUB_LAPSE_PRICE;
     });
+  }
+
+  getIncrementalPrice() {
     this.incrementalPrice = this.list.dataArray.map((sub) => {
       if (
         (sub.SLC_START && sub.SLC_END <= sub.SUB_LAPSE_END) ||
@@ -109,7 +120,9 @@ export class UserHomeComponent implements OnInit {
       }
       return undefined;
     });
+  }
 
+  getDecrementalPrice() {
     this.decrementalPrice = this.list.dataArray.map((sub) => {
       if (sub.SLC_START && sub.SLC_START <= sub.SUB_LAPSE_END) {
         return sub.SUB_LAPSE_PRICE - sub.SLC_PRICE;
@@ -118,12 +131,7 @@ export class UserHomeComponent implements OnInit {
     });
   }
 
-  ngOnInit() { }
-  navigate() {
-    this.router.navigate(["../", "login"], { relativeTo: this.actRoute });
-  }
-
-  goToDoc(link: string) {
+  goToPlatform(link: string) {
     window.open(link, "_blank");
   }
 }
